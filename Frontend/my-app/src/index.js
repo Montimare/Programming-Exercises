6d33992c694b0cdb3ee4f7addbfc18b0c59029c9
astralvoid-dev <149061265+astralvoid-dev@users.noreply.github.com>
2024-06-27 18:30:19 +0200

    Added path to user registration

--- Frontend/my-app/src/index.js
+++ Frontend/my-app/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import UserSelectionComponent from './components/UserSelectionComponent';
+import UserRegistrationComponent from './components/UserRegistrationComponent';
 import CalendarComponent from "./components/CalendarComponent";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -12,6 +13,7 @@ root.render(
     <Router>
       <Routes>
         <Route path="/" element={<UserSelectionComponent />} />
+        <Route path="/register" element={<UserRegistrationComponent />} />
         <Route path="/calendar" element={<CalendarComponent />} />
       </Routes>
     </Router>
