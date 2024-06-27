82d741fdba1624ff3bbd84e7962dc5e1222181af
astralvoid-dev <149061265+astralvoid-dev@users.noreply.github.com>
2024-06-27 21:12:29 +0200

    Established connection to backend and removed CORS

--- Frontend/my-app/src/components/WebService.js
+++ Frontend/my-app/src/components/WebService.js
@@ -1,9 +1,14 @@
 import axios from "axios";
 
-const fetchUsers = () => {
-    const axios = require('axios');
+export const fetchUsers = async () => {
+    return await axios.get("http://127.0.0.1:8000/users/");
+};
 
-    return axios.get("http://127.0.0.1:8000/users/");
-}
+export const createUsers = async (content) => {
+    //marc ist sich nicht mehr sicher wie genau das geht, aber wenn moeglich wollen wir die daten im body senden.
+    return await axios.post("http://127.0.0.1:8000/users/");
+};
 
-export default fetchUsers;
+export const fetchEvents = async () => {
+    return await axios.get("http://127.0.0.1:8000/events/");
+};
