441603e35477bab5bed7b944578c188fcd6025ba
astralvoid-dev <149061265+astralvoid-dev@users.noreply.github.com>
2024-07-02 21:08:29 +0200

    Usernames, emails and groups can be visualised in frontend using web requests

--- Frontend/my-app/src/Services/WebService.js
+++ Frontend/my-app/src/Services/WebService.js
@@ -19,4 +19,35 @@ export const fetchEventsByUser = async (userID) => {
 
 export const fetchEventListsByUser = async () => {
     return await axios.get("http://localhost:8000/users/" + userID + "/eventlists/");
+};
+
+export const fetchNotificationsByUser = async (userID) => {
+    return await axios.get("http://localhost:8000/users/" + userID + "/notifications/");
+};
+
+export const fetchGroupsByUser = async (userID) => {
+    return await axios.get("http://localhost:8000/users/" + userID + "/groups/");
 }
+
+// POST Functions
+export const createUsers = async (username, email) => {
+    //marc ist sich nicht mehr sicher wie genau das geht, aber wenn moeglich wollen wir die daten im body senden.
+    console.log("username:" + username);
+    console.log("email: " + email);
+
+    try {
+        await axios.post(
+            "http://127.0.0.1:8000/users/",
+            {
+                name: username,
+                email: email
+            }
+        );
+        // Return the response data or a success message to be displayed to the user
+        return 0; // or return "User created successfully!";
+    } catch (error) {
+        console.error("An error occurred:", error);
+        // Return an error message to be displayed to the user
+        return 1;
+    }
+};
