614571d8ed9123c387272f63041a8f22b9188bb4
astralvoid-dev <149061265+astralvoid-dev@users.noreply.github.com>
2024-07-03 16:37:28 +0200

    Created group popup window

--- Frontend/my-app/src/components/MenuSidebarComponent.js
+++ Frontend/my-app/src/components/MenuSidebarComponent.js
@@ -5,12 +5,19 @@ import GroupsIcon from "@mui/icons-material/Groups"
 import Divider from "@mui/material/Divider"
 import { Link, useLocation } from "react-router-dom";
 import { fetchGroupsByUser } from "../Services/WebService";
+import GroupPopupComponent from "./GroupPopupComponent"
 
 const MenuSidebarComponent = ({ selectedUserID }) => {
     const location = useLocation();
     const { username, email } = location.state || "";
     const [groups, setGroups] = useState([]);
+    const [selectedGroupID, setSelectedGroupID] = useState();
+    const [selectedGroupName, setSelectedGroupName] = useState();
     const [loading, setLoading] = useState(true);
+    const [openPopup, setOpenPopup] = useState(false);
+
+    useEffect(() => {}, [selectedGroupName]); // Dependency on selectedGroupName
+    useEffect(() => {}, [selectedGroupID]); // Dependency on selectedGroupID
 
     useEffect(() => {
         const getGroups = async () => {
@@ -27,7 +34,7 @@ const MenuSidebarComponent = ({ selectedUserID }) => {
 
         getGroups();    // call the async function
     }, []); // Empty dependency array means this effect runs only once
-    
+
     if (loading) {
         return (
             <div className="LoadingContainer">
@@ -36,38 +43,61 @@ const MenuSidebarComponent = ({ selectedUserID }) => {
         );
     }
 
+    const handleOpenPopup = (groupName, groupID) => {
+        setSelectedGroupName(groupName);
+        setSelectedGroupID(groupID);
+        setOpenPopup(true);
+    }
+
+    const handleClosePopup = () => {
+        setOpenPopup(false);
+    }
+
     return (
-        <Box
-            sx={{
-                width: 250
-            }}
-            role="presentation"
-        >
-            <List>
-                <ListItem>
-                    <List>
-                        <ListItem><PersonIcon /><b>User</b></ListItem>
-                        <ListItem>user@user.user</ListItem>
-                    </List>
-                </ListItem>
-                <Divider />
-                <ListItem><GroupsIcon /><b>Groups</b></ListItem>
-                <Divider variant="middle"/>
-                <ListItem>
-                    <List>
-                        {groups.map(group => (
-                            <ListItem key={group.id} value={group.id}>{group.name}</ListItem>
-                        ))}
-                    </List>
-                </ListItem>
-                <Divider />
-                <ListItem>
-                    <Link to={"/"}>
-                    <Button variant="text" color="error">Log out</Button>
-                    </Link>
-                </ListItem>
-            </List>
-        </Box>
+        <>
+            <Box
+                sx={{
+                    width: 250
+                }}
+                role="presentation"
+            >
+                <List>
+                    <ListItem>
+                        <List>
+                            <ListItem><PersonIcon /><b>{username}</b></ListItem>
+                            <ListItem>{email}</ListItem>
+                        </List>
+                    </ListItem>
+                    <Divider />
+                    <ListItem><GroupsIcon /><b>Groups</b></ListItem>
+                    <Divider variant="middle" />
+                    <ListItem>
+                        <List>
+                            {groups.map(group => (
+                                <ListItem key={group.id} value={group.id}>
+                                    <Button onClick={() => handleOpenPopup(group.name, group.id)}>{group.name}</Button>
+                                </ListItem>
+                            ))}
+                            <ListItem>
+                                <Button>Create new group...</Button>
+                            </ListItem>
+                        </List>
+                    </ListItem>
+                    <Divider />
+                    <ListItem>
+                        <Link to={"/"}>
+                            <Button variant="text" color="error">Log out</Button>
+                        </Link>
+                    </ListItem>
+                </List>
+            </Box>
+            {openPopup && (<GroupPopupComponent
+                openPopup={openPopup}
+                handleClosePopup={handleClosePopup}
+                groupName={selectedGroupName}
+                groupID={selectedGroupID}
+            />)}
+        </>
     );
 }
 
