47551f7476811a005794506e61c0490b3a227e35
Montimare <69976369+Montimare@users.noreply.github.com>
2024-07-06 22:03:12 +0200

    updated docker compose and tried to set up entrypoint for backend

---
+++ Backend/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "makemigrations..."
+python manage.py makemigrations
+
+# Apply database migrations
+echo "Applying database migrations..."
+python manage.py migrate
+
+# Start server
+echo "Starting server..."
+python manage.py runserver 0.0.0.0:8000
