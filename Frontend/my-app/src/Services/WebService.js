2639c83c97d08f8b556dd23d344389c984d61354
Montimare <69976369+Montimare@users.noreply.github.com>
2024-07-08 21:15:13 +0200

    fixed weird merging conflicts

--- Frontend/my-app/src/Services/WebService.js
+++ Frontend/my-app/src/Services/WebService.js
@@ -9,11 +9,6 @@ export const fetchUsers = async () => {
     return await axios.get("http://127.0.0.1:8000/users/");
 };
 
-export const createUsers = async (content) => {
-    //marc ist sich nicht mehr sicher wie genau das geht, aber wenn moeglich wollen wir die daten im body senden.
-    return await axios.post("http://127.0.0.1:8000/users/");
-};
-
 export const fetchEvents = async () => {
     return await axios.get("http://127.0.0.1:8000/events/");
 };
@@ -22,7 +17,7 @@ export const fetchEventsByUser = async (userID) => {
     return await axios.get("http://localhost:8000/users/" + userID + "/events/");
 };
 
-export const fetchEventListsByUser = async () => {
+export const fetchEventListsByUser = async (userID) => {
     return await axios.get("http://localhost:8000/users/" + userID + "/eventlists/");
 };
 
