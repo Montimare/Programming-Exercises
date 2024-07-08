b133c4a0e46c3130a5fc5f2090bbd2c383a874b9
Montimare <69976369+Montimare@users.noreply.github.com>
2024-07-08 21:49:47 +0200

    testing

--- Backend/mysite/mysite/settings.py
+++ Backend/mysite/mysite/settings.py
@@ -28,7 +28,10 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '192.168.178.2',
+    'localhost'
+]
 
 CORS_ALLOW_ALL_ORIGINS = DEBUG
 
